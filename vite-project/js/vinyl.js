0eece8262f35852589be51ab3c6f39e8ab323aa5
dieuhan <dieuhan.mut@gmail.com>
2023-11-16 13:14:04 -0500

    fwETEGT

--- vite-project/js/vinyl.js
+++ vite-project/js/vinyl.js
@@ -1,2 +1,35 @@
-const vinyls = ["The Bends", "Guts"]
+const vinyls = [
+    {
+        Name: "The Bends",
+        Artist: "Radiohead", 
+        Price: "$28.51",
+        Genre: "Alternative Rock"  
+    },
+    {
+        Name: "French Exit",
+        Artist: "TV Girl",
+        Price: "$25.00",
+        Genre: "Indie Pop"
+    },
+    {
+        Name: "Folklore: The Long Pond Studio Sessions",
+        Artist: "Taylor Swift",
+        Price: "$42.99",
+        Genre: "Indie Folk"
+    },
+    {
+        Name: "Selfish Machines",
+        Artist: "Pierce The Veil",
+        Price: "$51.49",
+        Genre: "Rock"
+    },
+    {
+        Name: "Hard to Imagine The Neighbourhood Ever Changing",
+        Artist: "The Neighbourhood",
+        Price: "47.92",
+        Genre: "Electropop"
+    },
+];
+
 console.log(vinyls);
+    
